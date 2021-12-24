91fd3d2445a275b6d2f20ca01b2f598ee18dbf45
Lý Ngọc Hữu <huuln9@gmail.com>
2021-12-24 10:08:41 +0700

    update ui list reson

--- lib/src/views/setting.dart
+++ lib/src/views/setting.dart
@@ -9,7 +9,7 @@ class Setting extends StatefulWidget {
 }
 
 class _SettingState extends State<Setting> {
-  bool english = false;
+  bool english = Get.locale == const Locale('en', 'US') ? true : false;
 
   @override
   Widget build(BuildContext context) {
@@ -36,21 +36,30 @@ class _SettingState extends State<Setting> {
             ),
           ),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Padding(padding: EdgeInsets.only(top: 10)),
-            ListTile(
-              leading: const Icon(Icons.language, color: Colors.black),
-              title: Text(
-                "English".tr,
-                style: const TextStyle(fontSize: 18),
-              ),
-              trailing: Padding(
-                padding: const EdgeInsets.only(right: 8.0, bottom: 8.0),
-                child: IconButton(
-                  icon: english
+        body: ScrollConfiguration(
+          behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+          child: ListView(
+            children: [
+              const Padding(padding: EdgeInsets.only(top: 10)),
+              ListTile(
+                onTap: () {
+                  setState(() {
+                    english = !english;
+                  });
+                  if (english) {
+                    Get.updateLocale(const Locale('en', 'US'));
+                  } else {
+                    Get.updateLocale(const Locale('vi', 'VN'));
+                  }
+                },
+                leading: const Icon(Icons.language, color: Colors.black),
+                title: Text(
+                  "English".tr,
+                  style: const TextStyle(fontSize: 18),
+                ),
+                trailing: Padding(
+                  padding: const EdgeInsets.only(right: 8.0, bottom: 8.0),
+                  child: english
                       ? const Icon(
                           Icons.toggle_on,
                           size: 50,
@@ -61,82 +70,66 @@ class _SettingState extends State<Setting> {
                           size: 50,
                           color: Colors.black54,
                         ),
-                  onPressed: () {
-                    setState(() {
-                      english = !english;
-                    });
-                    if (english) {
-                      Get.updateLocale(const Locale('en', 'US'));
-                    } else {
-                      Get.updateLocale(const Locale('vi', 'VN'));
-                    }
-                  },
                 ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-              child: Divider(thickness: 1.0),
-            ),
-            ListTile(
-              leading: const Icon(Icons.notifications, color: Colors.black),
-              title: Text(
-                "Enable notifications".tr,
-                style: const TextStyle(fontSize: 18),
+              const Padding(
+                padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                child: Divider(thickness: 1.0),
               ),
-              trailing: Padding(
-                padding: const EdgeInsets.only(right: 8.0, bottom: 8.0),
-                child: IconButton(
-                  icon: const Icon(
-                    Icons.toggle_off,
-                    size: 50,
-                    color: Colors.black54,
-                  ),
-                  onPressed: () => {},
+              ListTile(
+                leading: const Icon(Icons.notifications, color: Colors.black),
+                title: Text(
+                  "Enable notifications".tr,
+                  style: const TextStyle(fontSize: 18),
+                ),
+                trailing: const Padding(
+                  padding: EdgeInsets.only(right: 8.0, bottom: 8.0),
+                  child:
+                      Icon(Icons.toggle_off, size: 50, color: Colors.black54),
                 ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-              child: Divider(thickness: 1.0),
-            ),
-            ListTile(
-              leading: const Icon(Icons.message, color: Colors.black),
-              title: Text(
-                "Comment".tr,
-                style: const TextStyle(fontSize: 18),
+              const Padding(
+                padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                child: Divider(thickness: 1.0),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-              child: Divider(thickness: 1.0),
-            ),
-            ListTile(
-              leading: const Icon(Icons.phone_android, color: Colors.black),
-              title: Text(
-                "Version".tr,
-                style: const TextStyle(fontSize: 18),
+              ListTile(
+                leading: const Icon(Icons.message, color: Colors.black),
+                title: Text(
+                  "Comment".tr,
+                  style: const TextStyle(fontSize: 18),
+                ),
               ),
-              trailing: const Text(
-                "1.0.0",
-                style: TextStyle(fontSize: 18, color: Colors.blue),
+              const Padding(
+                padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                child: Divider(thickness: 1.0),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
-              child: Divider(thickness: 1.0),
-            ),
-            ListTile(
-              leading: const Icon(
-                Icons.ondemand_video,
-                color: Colors.black,
+              ListTile(
+                leading: const Icon(Icons.phone_android, color: Colors.black),
+                title: Text(
+                  "Version".tr,
+                  style: const TextStyle(fontSize: 18),
+                ),
+                trailing: const Text(
+                  "1.0.0",
+                  style: TextStyle(fontSize: 18, color: Colors.blue),
+                ),
               ),
-              title: Text(
-                "Instructional clips".tr,
-                style: const TextStyle(fontSize: 18),
+              const Padding(
+                padding: EdgeInsets.only(left: 10, right: 10, bottom: 10),
+                child: Divider(thickness: 1.0),
               ),
-            ),
-          ],
+              ListTile(
+                leading: const Icon(
+                  Icons.ondemand_video,
+                  color: Colors.black,
+                ),
+                title: Text(
+                  "Instructional clips".tr,
+                  style: const TextStyle(fontSize: 18),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
